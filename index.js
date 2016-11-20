10cf9039bd390d4220e380a5fb274a40d9eec4eb
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-11-20 18:57:46 -0300

    adding multiplayer system that adds & removes users at a given spawn point

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 require('aframe');
 require('aframe-text-component');
 
+let io = require('socket.io-client');
+
 // https://github.com/scenevr/interpolation-component
 // https://github.com/ngokevin/aframe-firebase-component
+
+AFRAME.registerSystem('multiplayer', {
+  schema: {
+    spawnPoint: { type: 'vec3' }
+  },
+  init() {
+    this._scene = document.querySelector('a-scene');
+
+    let socket = io('http://localhost:1338');
+    socket.on('join', (id) => {
+      let entity = document.createElement('a-entity');
+      entity.setAttribute('position', this.data.spawnPoint);
+      entity.setAttribute('text', `text: ${id}`);
+      entity.setAttribute('material', 'color: #880000');
+      entity.setAttribute('id', id);
+
+      this._scene.appendChild(entity);
+    });
+
+    socket.on('leave', (id) => {
+      let user = document.querySelector(`#${id}`);
+      this._scene.removeChild(user);
+    });
+  }
+});
